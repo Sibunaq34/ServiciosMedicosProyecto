39585c118ae9ddbc137f04cb488f14bc5727816c
Antony Cervantes <antonny22c.c@gmail.com>
2026-06-07 22:45:35 -0600

    Parametros corrigidos

--- ServiciosMedicos/Pages/Gerente/Parametros.cshtml.cs
+++ ServiciosMedicos/Pages/Gerente/Parametros.cshtml.cs
@@ -17,17 +17,67 @@ namespace ServiciosMedicos.Pages.Parametros
         [BindProperty]
         public ParametroEntidad Parametro { get; set; } = new();
 
-        public IEnumerable<ParametroEntidad>
-            ListaParametros
-        { get; set; }
-                = Enumerable.Empty<ParametroEntidad>();
+        public IEnumerable<ParametroEntidad> ListaParametros
+        { get; set; } = Enumerable.Empty<ParametroEntidad>();
 
         public string? Mensaje { get; set; }
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(int? id)
         {
             ListaParametros =
                 await _parametro.Listar();
+
+            if (id.HasValue)
+            {
+                var parametro =
+                    await _parametro.ObtenerPorId(id.Value);
+
+                if (parametro != null)
+                {
+                    Parametro = parametro;
+                }
+            }
+        }
+
+        public async Task<IActionResult> OnPostGuardar()
+        {
+            bool resultado;
+
+            if (Parametro.IdParametro == 0)
+            {
+                resultado =
+                    await _parametro.Insertar(Parametro);
+
+                TempData["Mensaje"] =
+                    $"Insertar: {resultado}";
+            }
+            else
+            {
+                resultado =
+                    await _parametro.Actualizar(Parametro);
+
+                TempData["Mensaje"] =
+                    $"Actualizar: {resultado}";
+            }
+
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostEliminar(int id)
+        {
+            try
+            {
+                await _parametro.Eliminar(id);
+
+                TempData["Mensaje"] =
+                    "Parámetro eliminado correctamente.";
+            }
+            catch (Exception ex)
+            {
+                TempData["Error"] = ex.Message;
+            }
+
+            return RedirectToPage();
         }
     }
 }
